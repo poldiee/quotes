d5fd832e08a35719310ae08574146539115aa909
poldiee <nessidave@gmail.com>
2022-04-12 15:26:13 +0300

    posts time postage duration addeing

--- src/app/date-pipe.pipe.ts
+++ src/app/date-pipe.pipe.ts
@@ -5,11 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown []): unknown {
-    transform(value, Date); number {
-      let today:Date = new Date();
-    return null;
-  }
+  // transform(value: any, ...args: any []): any {
+    // transform(value, Date); number {
+      transform(value: any): number {
+        let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(todayWithNoTime - value)
+    const secondsInADay= 86400;
+
+    var dateDifferenceSeconds=dateDifference*0.001; 
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
+
+    if (dateCounter >= 1 && todayWithNoTime > value){
+        return dateCounter;
+    }else{
+        return 0;
+    }
+
+      
 
   }
 }
