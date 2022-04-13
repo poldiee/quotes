da03a8c4d7a6d4d592a3688a71ef0832a16140a0
poldiee <nessidave@gmail.com>
2022-04-13 10:45:05 +0300

    date of quote posting

--- src/app/date-pipe.pipe.ts
+++ src/app/date-pipe.pipe.ts
@@ -1,27 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'datePipe'
+  name: 'datePipe',
 })
 export class DatePipePipe implements PipeTransform {
-
   // transform(value: any, ...args: any []): any {
-    // transform(value, Date); number {
-      transform(value: any): number {
-        let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(todayWithNoTime - value)
-    const secondsInADay= 86400;
+  // transform(value, Date); number {
+  transform(value: any): number {
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInADay = 86400;
 
-    var dateDifferenceSeconds=dateDifference*0.001; 
-    var dateCounter = dateDifferenceSeconds/secondsInADay;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
 
-    if (dateCounter >= 1 && todayWithNoTime > value){
-        return dateCounter;
-    }else{
-        return 0;
+    if (dateCounter >= 1 && todayWithNoTime > value) {
+      return dateCounter;
+    } else {
+      return 0;
     }
-
-      
-
   }
 }
